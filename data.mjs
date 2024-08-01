251206fa03ed61cf2de8399f9e6279cd43bc1ab9
Lucas Pinto <lucasdamianpinto1987@gmail.com>
2024-08-01 19:30:41 +0200

    corregi los tildes del texto

--- data.mjs
+++ data.mjs
@@ -1,7 +1,7 @@
 export const title = "Node HTTP";
 export const tituloPagina = "Node Entorno de ejecucion";
-export const subtituloPagina = "Aspectos técnicos";
+export const subtituloPagina = "Aspectos tecnicos";
 export const description =
-  "Node.js funciona con un modelo de evaluación de un único hilo de ejecución, usando entradas y salidas asíncronas las cuales pueden ejecutarse concurrentemente en un número de hasta cientos de miles sin incurrir en costos asociados al cambio de contexto.8​ Este diseño de compartir un único hilo de ejecución entre todas las solicitudes atiende a necesidades de aplicaciones altamente concurrentes, en el que toda operación que realice entradas y salidas debe tener una función callback. Un inconveniente de este enfoque de único hilo de ejecución es que Node.js requiere de módulos adicionales como cluster9​ para escalar la aplicación con el número de núcleos de procesamiento de la máquina en la que se ejecuta. descripción de mi sitio web.";
+  "Node.js funciona con un modelo de evaluacion de un unico hilo de ejecucion, usando entradas y salidas asincronas las cuales pueden ejecutarse concurrentemente en un numero de hasta cientos de miles sin incurrir en costos asociados al cambio de contexto.​ Este diseno de compartir un unico hilo de ejecucion entre todas las solicitudes atiende a necesidades de aplicaciones altamente concurrentes, en el que toda operacion que realice entradas y salidas debe tener una funcion callback. Un inconveniente de este enfoque de unico hilo de ejecucion es que Node.js requiere de modulos adicionales como cluster​ para escalar la aplicación con el numero de nucleos de procesamiento de la maquina en la que se ejecuta.";
 export const copyright =
   "Lucas Damian Pinto . Todos los derechos reservados @2024.";
